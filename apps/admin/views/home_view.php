013b32248feaed36c2ac9a4d197bd826a384b631
jlallana <juan.manuel.lallana@gmail.com>
2015-04-22 20:35:03 +0000

    Se crea aplicacion vacia para el administrador de contenido.

---
+++ apps/admin/views/home_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Demostración</title>
+	</head>
+	<body>
+		<h1>Demostración</h1>
+		<p>Aplicación de ejemplo con Code Igniter.</p>
+	</body>
+</html>
